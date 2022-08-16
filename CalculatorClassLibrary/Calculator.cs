4aaf7a75cfdcd8573d2caff67d467adf0c247fb6
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-08-16 16:01:06 +0530

    ml

--- CalculatorClassLibrary/Calculator.cs
+++ CalculatorClassLibrary/Calculator.cs
@@ -5,7 +5,11 @@
         // BL
         public static int FindSum(int a, int b)
         {
+            // both the input must me positive, non zero and even numbers only
+
             return a + b;
         }
+
+        // find the difference
     }
 }
