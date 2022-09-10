d0a96898805ff87e4b6f936c22463f7b74b54615
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-09-10 12:00:15 +0530

    linq

---
+++ LanguageEnhancementsForLINQ/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LanguageEnhancementsForLINQ
+{
+    internal class Program
+    {
+
+        int a = 10;
+        static void Main(string[] args)
+        {
+            // Local variable type inference
+            var a = 10;
+            var d = 10.5;
+            var str = "abcd";
+
+            //Emp e1 = new Emp();
+            //e1.EmpNo = 111;
+            //e1.Name = "Ramesh";
+            //e1.Salary = 50000;
+
+            // Object Initialization Syntax
+            //Emp e2 = new Emp() { EmpNo = 222, Name = "Kiran", Salary = 60000 };
+            var e2 = new { EmpNo = 222, Name = "Kiran", Salary = 60000 };
+            // Anoymous Objects
+
+            // Extension Methods
+            string data = "secret data";
+
+            data.ToUpper();
+            data = StringUtil.Encrypt(data);
+            data.Encrypt();
+            // SQL SELECT
+            // from select where orderby .Select .Where .Orderby
+
+        }
+    }
+
+    static class StringUtil
+    {
+        public static string Encrypt(this string data)
+        {
+            return "@#$@#DWERWE@#$@#%#$^@#%!#$^!#$^!#$^";
+        }
+    }
+
+    //class Emp
+    //{
+    //    public int EmpNo { get; set; }
+    //    public string Name { get; set; }
+    //    public int Salary { get; set; }
+    //}
+}
---
+++ LinqDemos/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LinqDemos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linq To Objects");
+            List<int> numbers = new List<int> { 23, 53, 23, 64, 24, 75, 22, 77, 34 };
+            // Get all even numbers into another array and display
+            // traditional way
+            var evens = new List<int>();
+            foreach (var item in numbers)
+            {
+                if (item % 2 == 0)
+                    evens.Add(item);
+            }
+            foreach (var item in evens)
+            {
+                //Console.WriteLine(item);
+            }
+            // table: numbers
+            // column: number
+            // SQL: select number from numbers where number mod 2 = 0;
+
+            // LINQ Expression
+            var evens2 = from n in numbers where n % 2 == 0 select n;
+            // LINQ Extension Methods
+            var evens3 = numbers.Where(n => n % 2 == 0);
+
+            foreach (var item in evens3)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
