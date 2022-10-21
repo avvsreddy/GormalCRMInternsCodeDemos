fd48e966de8b5e04a6c6137291dfb68a6fe1ed0e
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-10-21 13:22:06 +0530

    mvc

--- KnowledgeHubPortalMVCWebApplication/Controllers/CatagoriesController.cs
+++ KnowledgeHubPortalMVCWebApplication/Controllers/CatagoriesController.cs
@@ -42,8 +42,45 @@ namespace KnowledgeHubPortalMVCWebApplication.Controllers
                 return View();
             }
             repo.Create(catagory);
+
+
+            //ViewBag.Message = "sdfsdfsd";
+            //ViewData["Message"] = "sdfsdfsd";
+            TempData["Message"] = $"Catagory {catagory.Name} created successfully";
+
             return RedirectToAction("Index");
         }
 
+        public IActionResult Delete(int id)
+        {
+            repo.Delete(id);
+            TempData["Message"] = $"Catagory Id {id} deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var catagoryToEdit = repo.GetCatagory(id);
+            return View(catagoryToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Catagory catagory)
+        {
+            // collects data from view and send to model
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            repo.Edit(catagory);
+
+
+            //ViewBag.Message = "sdfsdfsd";
+            //ViewData["Message"] = "sdfsdfsd";
+            TempData["Message"] = $"Catagory {catagory.CatagoryID} edited successfully";
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- KnowledgeHubPortalMVCWebApplication/Models/Entities/Catagory.cs
+++ KnowledgeHubPortalMVCWebApplication/Models/Entities/Catagory.cs
@@ -5,7 +5,7 @@ namespace KnowledgeHubPortalMVCWebApplication.Models.Entities
     public class Catagory
     {
         public int CatagoryID { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter catagory name")]
         public string Name { get; set; }
         [MaxLength(500)]
         public string Description { get; set; }
